c83734b10d76fe4386d099366a2d51bb6983ff69
dfokin <dmytro.fokin@credenza3.com>
2023-12-20 18:59:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+  const clientIp = req.connection.remoteAddress;
+  console.log('Ip': clientIp)
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!');
